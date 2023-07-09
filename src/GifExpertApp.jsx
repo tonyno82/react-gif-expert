f09a542888c3418dbccae007aa259b346607bb2a
Antonio Rodriguez <antonio.rodriguez@rodama.com>
2023-07-09 21:32:34 +0200

    First commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,37 @@
+
+import { useState } from "react";
+import { AddCategory, GiftGrid } from "./components";
+
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch'])
+
+  const onAddCategory = ( newCategory ) => {
+
+    if (categories.includes(newCategory)) return;
+
+    setCategories([newCategory, ...categories])
+
+  }
+
+  return (
+    <>
+        <h1>GifExpertApp</h1>
+        <AddCategory 
+          onNewCategory={ (value) => onAddCategory(value) }
+        />
+
+          { 
+            categories.map( category => (
+              <GiftGrid 
+                key={ category } 
+                category={category}
+              />
+              )
+            )
+          }
+
+
+    </>
+  )
+}
